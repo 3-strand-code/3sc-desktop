d1eaf68b67a73bde846db2afa9340e16ab7682d9
levithomason <me@levithomason.com>
2016-03-13 08:49:19 -0700

    test(nightmare): add nightmare example test

---
+++ test/nightmare/yahoo.js
@@ -0,0 +1,11 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+
+nightmare
+  .goto('http://yahoo.com')
+  .type('input[title="Search"]', 'github nightmare')
+  .click('#uh-search-button')
+  .wait('#main')
+  .evaluate(() => document.querySelector('#main .searchCenterMiddle li a').href)
+  .end()
+  .then((x) => console.log(x))
