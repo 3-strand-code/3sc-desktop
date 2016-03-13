eb5ae2a29e09b9d31aa451e1e02917e44c958ecd
levithomason <me@levithomason.com>
2016-03-12 22:32:18 -0800

    feat(Step): add markdown description

--- app/components/Step.js
+++ app/components/Step.js
@@ -1,18 +1,19 @@
-import React, { Component, PropTypes } from 'react'
-// import marked from 'marked'
-import { Segment } from 'stardust'
 import cx from 'classnames'
+import React, { Component, PropTypes } from 'react'
+import { Segment } from 'stardust'
+
+import Markdown from './Markdown/Markdown'
 
 export default class Step extends Component {
   static propTypes = {
     title: PropTypes.string,
     check: PropTypes.bool,
     /** path to markdown file */
-    instructions: PropTypes.string,
+    description: PropTypes.string,
   }
 
   render() {
-    const { title, check, instructions } = this.props
+    const { title, check, description } = this.props
     const iconClasses = cx({
       'green check': check,
       'grey square outline': !check,
@@ -20,13 +21,11 @@ export default class Step extends Component {
     const segmentClasses = cx({
       'secondary': check,
     })
+    const renderedDescription = !check && description && <Markdown source={description} />
     return (
       <Segment className={segmentClasses}>
         <i className={iconClasses} /> {title}
-        <p>
-          {/* TODO render markdown: marked(instructions) */}
-          {instructions}
-        </p>
+        {renderedDescription}
       </Segment>
     )
   }
